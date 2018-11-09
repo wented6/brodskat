acffdb81390f6289ca2df0747443425e97d20543
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-09 19:56:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "#"// البرفكس هنا
+var prefix = "bc"// البرفكس هنا
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
